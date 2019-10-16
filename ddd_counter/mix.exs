275aea136197491c8e997134f3fb9bca45f9304a
Mark Windholtz <mark@agiledna.com>
2019-10-16 13:49:08 -0400

    limbo-commit

--- ddd_counter/mix.exs
+++ ddd_counter/mix.exs
@@ -37,13 +37,14 @@ defmodule DddCounter.MixProject do
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.1"},
+      {:gettext, "~> 0.11"},
+      {:jason, "~> 1.0"},
+      {:limbo, "~> 0.1.4"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"},
-      {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
-      {:limbo, "~> 0.1.4"},
+      {:test_reflector, "~> 0.1.4"},
       {:plug_cowboy, "~> 2.0"}
     ]
   end
